9f18abffccbe65bf277151aeea5456f25ef04837
Abentancor <Angelbentancor@gmail.com>
2022-04-01 14:57:32 -0300

    slider responsive

---
+++ src/Components/Header2.jsx
@@ -0,0 +1,41 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faBars, faCartShopping} from '@fortawesome/free-solid-svg-icons'
+import React, { Component, useState } from 'react'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { Link, NavLink } from 'react-router-dom'
+
+export default class Header extends Component {
+    
+    render() {
+        library.add(faCartShopping, faBars)
+        
+        const [showMenu, setShowMenu] = useState(false)
+        const toggleMenu = () => {
+          setShowMenu(!showMenu)
+        }
+        const hidden = showMenu ? 'hidden' : '' 
+      
+        return (
+        <>
+        <div className='text-4xl text-white font-bold p-2 bg-slate-900 flex justify-between list-none'>
+          <Link to='/'>
+            <h1 className='ml-10 hover:scale-125'>Nike</h1> 
+          </Link>
+                <li onClick={toggleMenu} className='flex self-center order-last ring ring-sky-900 rounded-lg cursor-pointer p-0.5 mr-2 hover:scale-105 hover:ring-white sm:hidden'>
+                  <FontAwesomeIcon icon="fa-solid fa-bars" className='w-6 h-6' />
+                </li>
+            <ul className={`${hidden} flex justify-between flex-col sm:flex-row`}>
+              <NavLink to="/Products">
+                <li className='px-3 text-lg flex self-center hover:scale-125'>Productos</li>
+              </NavLink>
+              <NavLink to="/Contact">
+                <li className='px-3 text-lg flex self-center hover:scale-125 '>Contacto</li>
+              </NavLink>
+              <NavLink to='/Cart'>
+                <li className=' hover:scale-125 mr-10'><FontAwesomeIcon icon="fa-solid fa-cart-shopping" className='mx-4 w-6' />0</li>
+              </NavLink>
+            </ul>
+        </div>
+    </>
+    )
+}}
--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { DataContext } from '../Context/Context'
+import { NavLink } from 'react-router-dom';
 
 import {Swiper, SwiperSlide} from "swiper/react"
 import { Autoplay, Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import 'swiper/css/autoplay';
-import { NavLink } from 'react-router-dom';
 
 
 
@@ -27,14 +27,28 @@ export class Slider extends Component {
             }}
             autoplay={{delay:3000 }}
             className={`flex justify-between flex-colflex-row`}    
+            breakpoints={{
+                "@0.00": {
+                  slidesPerView: 1,
+                  spaceBetween: 10,
+                },
+                "@0.75": {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                "@1.00": {
+                  slidesPerView: 3,
+                  spaceBetween: 40,
+                },
+              }}
         >
                 
                 {
                     products.map(product =>(
-                        <SwiperSlide className='slider flex flex-row mx-auto' key={product._id}>
+                        <SwiperSlide className='slider flex flex-row mx-auto ' key={product._id}>
                             <div className='slider p-3 flex flex-col items-center '>
                                 <NavLink to={`/Product/${product._id}`}>
-                                    <img className='' src={product.src} alt={product.title} />
+                                    <img className='rounded-xl' src={product.src} alt={product.title} />
                                 </NavLink>
                                 <button className='mt-2 bg-slate-200 rounded-xl p-1 font-semibold ring ring-sky-900  hover:bg-slate-900 hover:text-white hover:ring-white scale-105 '>
                                      Add to cart
